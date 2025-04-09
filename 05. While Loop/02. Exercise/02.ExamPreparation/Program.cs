d6db3d13ce44afeeec0ef750d6fd6bd2f12da7dc
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-04-09 17:12:39 +0300

    Basics

---
+++ 05. While Loop/02. Exercise/02.ExamPreparation/Program.cs
@@ -0,0 +1,43 @@
+﻿//Input
+int allowPoorGrades = int.Parse(Console.ReadLine());
+
+//Calculations
+int probelmsCount = 0;
+string lastProblemName = "";
+int poorGrades = 0;
+int gradeSum = 0;
+
+while (true)
+{
+    string problemName = Console.ReadLine();
+    if (problemName != "Enough")
+    {
+        probelmsCount++;
+        lastProblemName = problemName;
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade <= 4)
+        {
+            poorGrades++;
+            if (poorGrades >= allowPoorGrades)
+            {
+                Console.WriteLine($"You need a break, {poorGrades} poor grades.");// Output
+                break;
+            }
+            gradeSum += grade;
+            continue;
+        }
+        if (grade > 4)
+        {
+            gradeSum += grade;
+        }
+    }
+
+    if (problemName == "Enough")
+    {
+        Console.WriteLine($"Average score: {(double)gradeSum / probelmsCount:f2}");
+        Console.WriteLine($"Number of problems: {probelmsCount}");
+        Console.WriteLine($"Last problem: {lastProblemName}");
+        break;
+    }
+}
