d6a0edd92083cf33399270877cbe06eace297171
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-04-21 13:40:20 +0300

    Basics

---
+++ 05. While Loop/02. Exercise/07.Moving/Program.cs
@@ -0,0 +1,27 @@
+﻿//Input
+int width = int.Parse(Console.ReadLine());
+int length = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+string input = "";
+
+//Calculations
+int freeSpace = width * length * height;
+
+while ((input = Console.ReadLine()) != "Done")
+{
+    int cartons = int.Parse(input);
+    freeSpace -= cartons;
+
+    if (freeSpace <= 0)
+    {
+        //Output
+        Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace)} Cubic meters more.");
+        break;
+    }
+}
+
+//Output
+if (input == "Done")
+{
+    Console.WriteLine($"{freeSpace} Cubic meters left.");
+}
